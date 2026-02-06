61d0ac5fa2b723ab76082e298df525526816548c
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2026-02-06 16:19:07 -0500

    standardize md5 buffer

--- Stalker.Gamma/Utilities/Md5Utility.cs
+++ Stalker.Gamma/Utilities/Md5Utility.cs
@@ -1,3 +1,4 @@
+using System.Buffers;
 using System.Security.Cryptography;
 
 namespace Stalker.Gamma.Utilities;
@@ -14,20 +15,30 @@ public static class Md5Utility
         await using var stream = File.OpenRead(filePath);
 
         var fileSize = stream.Length;
-        var buffer = new byte[81920]; // 80 KB buffer
-        long totalBytesRead = 0;
-        int bytesRead;
+        var buffer = ArrayPool<byte>.Shared.Rent(BufferLen);
 
-        while ((bytesRead = await stream.ReadAsync(buffer, cancellationToken)) > 0)
+        try
         {
-            md5.TransformBlock(buffer, 0, bytesRead, null, 0);
-            totalBytesRead += bytesRead;
-            onProgress.Invoke((double)totalBytesRead / fileSize);
+            long totalBytesRead = 0;
+            int bytesRead;
+
+            while ((bytesRead = await stream.ReadAsync(buffer, cancellationToken)) > 0)
+            {
+                md5.TransformBlock(buffer, 0, bytesRead, null, 0);
+                totalBytesRead += bytesRead;
+                onProgress.Invoke((double)totalBytesRead / fileSize);
+            }
+
+            // Finalize the hash computation
+            md5.TransformFinalBlock([], 0, 0);
+
+            return Convert.ToHexStringLower(md5.Hash!);
         }
-
-        // Finalize the hash computation
-        md5.TransformFinalBlock([], 0, 0);
-
-        return Convert.ToHexStringLower(md5.Hash!);
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(buffer);
+        }
     }
+
+    private const int BufferLen = 1024 * 1024;
 }
