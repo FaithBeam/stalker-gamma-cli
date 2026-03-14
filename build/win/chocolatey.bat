2ee8fc5f29d0d4b1cdb019abf1ed071e8737d324
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2026-03-14 06:30:59 -0400

    fix chocolatey again

--- build/win/chocolatey.bat
+++ build/win/chocolatey.bat
@@ -3,4 +3,4 @@ SET version=%~1
 SET choco_api_key=%~2
 SET path_to_archive=%~3
 if not defined version SET version=1.0.0
-powershell.exe -noprofile -executionpolicy bypass -file %script_dir%chocolatey.ps1 -Version %version% -ChocolateyApiKey %choco_api_key% -PathToArchive path_to_archive
+powershell.exe -noprofile -executionpolicy bypass -file %script_dir%chocolatey.ps1 -Version %version% -ChocolateyApiKey %choco_api_key% -PathToArchive %path_to_archive%
